59e6407bf74f698d3ed3744331616289f68be474
s3anzi3 <s3anzie@gmail.com>
2024-01-17 14:33:23 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,17 @@
+// setup nav
+const navBtn = document.getElementById("nav-btn");
+const navbar = document.getElementById("navbar");
+const navClose = document.getElementById("nav-close");
+// show nav
+navBtn.addEventListener("click", () => {
+  navbar.classList.add("showNav");
+});
+// close nav
+navClose.addEventListener("click", () => {
+  navbar.classList.remove("showNav");
+});
+window.onload = function() {
+  const test= document.getElementById("test");
+  test.innerHTML= new Date().getFullYear();
+}
+
